82f63bc3c95b6a0d86b8f6398a885f47237dc7a7
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-31 17:47:40 +0100

    Milestone 1 completa

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':<>,.?\/";
+    $passLength = $_GET['numeroPassword'];
+
+    function getPassword($input, $numero){
+        $password = '';
+        for($i > 0; $i < $numero; $i++){
+            $password .= $input[rand(0, strlen($input))];
+        }
+        return $password;
+        
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,9 +21,26 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Strong Password Generator</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style/style.css">
     </head>
     <body>
-        
+        <div class="container-fluid h-100vh d-flex flex-column align-items-center justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <form action="./index.php" method="GET" class="form-container">
+                        <input type="number" name="numeroPassword">
+                        <button type="submit">Invia</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-container">
+                        <h1><?php echo getPassword($characters, $passLength)?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
